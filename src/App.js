bb88e4296133ee0720eec5615f3b4122d581e8ad
mdpabel <mdpabel385@gmail.com>
2021-08-29 12:43:25 +0600

    Static javascript testing tools

---
+++ src/App.js
@@ -0,0 +1,17 @@
+'use-strict';
+
+const username = 'MD Pabel';
+
+if (10 > 9) {
+  // var n = 10;
+}
+
+// console.log(n);
+
+typeof username == 'string';
+
+function printHello() {
+  console.log('Hello!');
+}
+
+printHello();
